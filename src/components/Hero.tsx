55643283eed0bacdf0e0314267109252713f45f9
Benoit LE DOUARIN <benoit.le-douarin@isocel.info>
2025-12-28 19:43:25 +0100

    feat: theme plus fun

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -10,6 +10,8 @@ const Hero = () => {
           <div className={styles.shape1}></div>
           <div className={styles.shape2}></div>
           <div className={styles.shape3}></div>
+          <div className={styles.shape4}></div>
+          <div className={styles.shape5}></div>
         </div>
       </div>
 
